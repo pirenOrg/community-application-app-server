e2a11ea5f177cf70a8466a2834f55c2160221b60
전준영 <wnsdud9476@DESKTOP-2VICEL1.localdomain>
2022-10-13 16:11:22 +0900

    [MODIFY] Follow module 수정

--- src/follows/follows.module.ts
+++ src/follows/follows.module.ts
@@ -5,12 +5,18 @@ import { FollowsService } from './follows.service';
 import { FollowsController } from './follows.controller';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/users/security/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Follow, User])
+    TypeOrmModule.forFeature([Follow, User]),
+    JwtModule.register({ secret: process.env.JWT_SECRET_KEY, signOptions: { expiresIn: '5h' } }),
+    PassportModule,
   ],
+  exports: [TypeOrmModule],
   controllers: [FollowsController],
-  providers: [FollowsService, UsersService]
+  providers: [FollowsService, UsersService, JwtStrategy],
 })
 export class FollowsModule {}
